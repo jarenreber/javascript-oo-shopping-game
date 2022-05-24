718965f0bff9fe00c9f7b1c43089d89408245f02
jarenreber <102092511+jarenreber@users.noreply.github.com>
2022-05-24 11:03:35 -0600

    adding function class for product

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -20,17 +20,15 @@ const player = {
   },
 };
 // Define the Product class - write the Constructor function for Product class here
-class Product {
+function Product(id, name, price, expiryDate) {
   id;
   name;
   price;
   expiryDate;
-  constructor(id, name, price, expiryDate) {
-    this.id = id;
-    this.name = name;
-    this.price = price;
-    this.expiryDate = expiryDate;
-  }
+  this.id = id;
+  this.name = name;
+  this.price = price;
+  this.expiryDate = expiryDate;
 }
 // Complete the dateDiff function
 const dateDiff = (date1, date2) => {
