618604d32101cb41f3ae3cc80a61f79a1b45db6f
jarenreber <102092511+jarenreber@users.noreply.github.com>
2022-06-03 14:22:52 -0600

    finished section 4

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -245,10 +245,18 @@ function loadMasterData() {
 }
 
 // Complete this function
-const findProductById = (id) => {};
+const findProductById = (id) => {
+  return (Product) => {
+    if (id === Product.id) {
+      return true;
+    } else return false;
+  };
+};
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => {
+  return Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+};
 
 const getProduct = (prodList, pId) => {
   return prodList.find(findProductById(pId));
